32ef9a1c6aeb55c4708dfa14143e539f328741f5
Mohammed Bazed Ali <mohdbazedali00@gmail.com>
2024-07-15 18:55:34 +0530

    day 1 tasks

---
+++ Day1/tasks.js
@@ -0,0 +1,38 @@
+var number = 30;
+console.log('Value = ',number);
+console.log('Type = ',typeof number);
+
+let string = "This is a string";
+console.log('Value = ',string);
+console.log('Type = ',typeof string);
+
+const bool = true;
+console.log('Value = ',bool);
+console.log('Type = ',typeof bool);
+
+const userprofile = {
+    name: "Mohammed Bazed Ali",
+    age: 24
+}
+console.log('Value = ',userprofile);
+console.log('Type = ',typeof userprofile);
+
+const array = [1, 2, 3, 4, 5];
+console.log('Value = ',array);
+console.log('Type = ',typeof array);
+
+let result = 17;
+console.log('initial let result = ',result);
+result = "This is a string";
+console.log('reintialized let result = ',result);
+
+let letvar = 10;
+console.log("Initial letvar:", letvar);
+
+letvar = 20;
+console.log("Reassigned letvar:", letvar);
+
+const constvar = 30;
+console.log("Initial constvar:", constvar);
+
+constvar = 40;
